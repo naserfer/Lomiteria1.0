797462c9d2326f8d20fedde6f7024d2c6f57ba10
Iván Ortiz <ivanxavi4@gmail.com>
2026-04-28 13:38:18 -0300

    app version update

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -3,7 +3,7 @@ export const LOGIN_STRINGS = {
   LOGIN_TITLE: 'KarúBox',
 } as const
 
-export const APP_VERSION = '1.1.5' as const
+export const APP_VERSION = '1.2.6' as const
 export const APP_VERSION_LABEL = `KarúBox - v${APP_VERSION}` as const
 
 export type LoginStrings = keyof typeof LOGIN_STRINGS
